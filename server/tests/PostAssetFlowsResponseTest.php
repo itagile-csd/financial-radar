c07b5a1169b26c483e4e89a203692d753a8b0bf8
Software Craftsman <csd@it-agile.de>
2017-02-21 16:59:16 +0100

    refactoring

--- server/tests/PostAssetFlowsResponseTest.php
+++ server/tests/PostAssetFlowsResponseTest.php
@@ -2,29 +2,29 @@
 
 class PostAssetFlowsResponseTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->json('PUT', '/assetFlows', array());
+        $this->json('POST', '/assetFlows', array('EmployeeID' => 'AW', 'Type' => 'User', 'Amount' => 42, 'Date' => time()));
+    }
 
     public function testShouldReturnCreatedResponse()
     {
-        $this->json('POST', '/assetFlows', array('amount' => 10));
         assertThat($this->response->getStatusCode(), equalTo(201));
     }
 
-    public function testSavingSingleDataset(){
-        $this->json('PUT', '/assetFlows', array());
-
-        $this->json('POST', '/assetFlows', array('EmployeeID' => 'AW', 'Type' => 'User', 'amount' => 42, 'Date' => time()));
-
+    public function testSavingSingleDataset()
+    {
         $this->json('GET', '/assetFlows');
         $content = json_decode($this->response->getContent(), true);
 
         assertThat($content[0]['EmployeeID'], is('AW'));
     }
 
-    public function testSavingMultipleDataset(){
-        $this->json('PUT', '/assetFlows', array());
-
-        $this->json('POST', '/assetFlows', array('EmployeeID' => 'AW', 'Type' => 'User', 'amount' => 42, 'Date' => time()));
-        $this->json('POST', '/assetFlows', array('EmployeeID' => 'ML', 'Type' => 'User', 'amount' => 700, 'Date' => time()));
+    public function testSavingMultipleDataset()
+    {
+        $this->json('POST', '/assetFlows', array('EmployeeID' => 'ML', 'Type' => 'User', 'Amount' => 700, 'Date' => time()));
 
         $this->json('GET', '/assetFlows');
         $content = json_decode($this->response->getContent(), true);
